c17d6724637d476bec6e65a9d2eded771c364ffd
Ian Wright <49083526+Landaman@users.noreply.github.com>
2025-02-12 19:24:02 -0500

    Use an Eslint setup that includes TypeScript-Eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,50 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+// @ts-check
 import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { dirname } from "path";
+import tseslint, { parser as tseslintParser } from "typescript-eslint";
+import { fileURLToPath } from "url";
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: dirname(fileURLToPath(import.meta.url)),
 });
 
-const eslintConfig = [
+export default tseslint.config(
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  eslintConfigPrettier,
+  {
+    rules: {
+      semi: "error",
+      "no-empty": "warn",
+      "@typescript-eslint/no-empty-function": "warn",
+      "prefer-const": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+      "@typescript-eslint/no-empty-interface": "warn",
+      "@typescript-eslint/no-shadow": [
+        "warn",
+        { builtinGlobals: true, hoist: "functions", allow: ["Image"] },
+      ],
+    },
+    languageOptions: {
+      parser: tseslintParser,
+      parserOptions: {
+        projectService: true,
+      },
+    },
+  },
+);
