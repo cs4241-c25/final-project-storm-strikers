2f2c594e58b84344bb7547821ad48bbefc00f245
asamarin22 <asamarin@wpi.edu>
2025-02-24 12:12:12 -0500

    cleaned up

--- src/app/admin/services/actions.ts
+++ src/app/admin/services/actions.ts
@@ -18,28 +18,6 @@ export async function createService(formData: FormData) {
   revalidatePath("/services");
 }
 
-// export async function deleteService(formData: FormData) {
-//   const serviceName = formData.get("name") as string;
-//   const query = { name: serviceName };
-//
-//   await services.deleteOne(query);
-//   revalidatePath("/services");
-// }
-// export async function editService(formData: FormData) {
-//   const service = {
-//     name: formData.get("name") as string,
-//     specialities: (formData.get("specialities") as string).split(","),
-//     floor: (formData.get("floor") as string).split(","),
-//     suite: (formData.get("suite") as string).split(","),
-//     phone: formData.get("phone") as string,
-//     hours: formData.get("hours") as string,
-//     building: formData.get("building") as BuildingType,
-//   };
-//
-//   await services.insertOne(service);
-//   revalidatePath("/services");
-// }
-
 export async function editService(formData: FormData) {
   const serviceId = formData.get("name") as string;
   if (!serviceId) {
